ab2d44e244706cd9023f471d2cca6401272950e9
Vladimir <109244014+mrVladim@users.noreply.github.com>
2022-08-11 01:42:57 +0300

    Add files via upload

---
+++ Task008.cs
@@ -0,0 +1,18 @@
+﻿int u = 1;
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа от 1 до " + N);
+bool not = true;
+while (u <= N)
+{
+    if (u % 2 != 1)
+    {
+        Console.Write(u + ", ");
+        not = false;
+    }
+    u++;
+}
+if (not)
+{
+    Console.WriteLine("Нет чётных чисел");
+}
