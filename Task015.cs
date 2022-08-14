d66ba44bd316b8d60fa5833fdf86ab3be2746e6d
Vladimir <109244014+mrVladim@users.noreply.github.com>
2022-08-14 22:41:02 +0300

    Add files via upload

---
+++ Task015.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void DayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("нет такого днгя недели");
+  }
+  else Console.WriteLine("(этот день выходной?) -> нет");
+}
+
+DayOfTheWeek(dayNumber);
