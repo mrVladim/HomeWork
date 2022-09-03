585c7ea813cc3d23f07a00179452de5d3dde43b1
Vladimir <109244014+mrVladim@users.noreply.github.com>
2022-09-04 00:57:30 +0300

    Add files via upload

---
+++ Task029.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите количество элементов");
+int a = Convert.ToInt32(Console.ReadLine());
+int [] numbers = new int[a];
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, a);
+    Console.Write(" " + Method (i) + " ");
+ }
+
+int Method (int a)
+{
+    return numbers[a];
+}
