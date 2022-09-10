3c9bf312c8ddbc4da13efa586fec8e5bbb890623
Vladimir <109244014+mrVladim@users.noreply.github.com>
2022-09-10 22:19:07 +0300

    Add files via upload

---
+++ Task035.cs
@@ -0,0 +1,16 @@
+﻿int N = 123;
+int[] a = new int[N];
+Random random = new Random();
+for (int i = 0; i < a.Length; i++)
+    a[i] = random.Next(0, 123);
+for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{a[i],4}");
+
+int count = 0;
+for (var i = 0; i < a.Length; i++)
+{
+    if (i > 10 && i < 99)
+        count++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine(count);
---
+++ Task037.cs
@@ -0,0 +1,26 @@
+﻿int J;
+Console.Write("Введите размерность массива: ");
+J = int.Parse(Console.ReadLine());
+double[] chisla = new double[J];
+int h;
+if (J % 2 == 0)
+{
+    h = J / 2;
+}
+else
+{
+    h = J / 2 + 1;
+}
+double[] resultat = new double[h];
+for (int i = 0; i < J; i++)
+{
+    Console.Write("Введите " + (i + 1) + " элемент массива" + " ");
+    chisla[i] = double.Parse(Console.ReadLine());
+}
+double k = 0;
+for (int i = 0, j = 0; i < J; j++, i += 2)
+{
+    k = (chisla[i] * chisla[i + 1]);
+    resultat[j] = k;
+    Console.Write(resultat[j] + " ");
+}
